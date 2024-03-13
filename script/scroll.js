48f3c4615c88338bad884437f360cb969be2ac4f
rustymotor <vmfpdlasz@gmail.com>
2024-03-13 18:16:04 +0900

    13

--- script/scroll.js
+++ script/scroll.js
@@ -1,12 +1,13 @@
 gsap.from('.content1',{
     scrollTrigger:
     {
+    duration:1,
     trigger:'.content1',
     start:'0% 60%', //첫번째는 요소기준, 두번째는 윈도우기준
     end:'100% 80%',
     markers:true,  //화면에 가이드선 표시
     id:'박스5',
-    scrub:1,
+    // scrub:1,
     },
     y:200,
     opacity:0,
@@ -14,25 +15,25 @@ gsap.from('.content1',{
 gsap.from('.content2',{
     scrollTrigger:
     {
-    trigger:'.content1',
+    trigger:'.content2',
     start:'0% 60%', //첫번째는 요소기준, 두번째는 윈도우기준
     end:'100% 80%',
     markers:true,  //화면에 가이드선 표시
     id:'박스5',
-    scrub:1,
+    // scrub:1,
     },
-    y:200,
+    y:300,
     opacity:0,
 });
 gsap.from('.content3',{
     scrollTrigger:
     {
-    trigger:'.content1',
-    start:'0% 30%', //첫번째는 요소기준, 두번째는 윈도우기준
+    trigger:'.content3',
+    start:'0% 60%', //첫번째는 요소기준, 두번째는 윈도우기준
     end:'100% 80%',
     markers:true,  //화면에 가이드선 표시
     id:'박스5',
-    scrub:1,
+    // scrub:1,
     },
     y:200,
     opacity:0,
@@ -40,12 +41,12 @@ gsap.from('.content3',{
 gsap.from('.content4',{
     scrollTrigger:
     {
-    trigger:'.content1',
+    trigger:'.content4',
     start:'0% 60%', //첫번째는 요소기준, 두번째는 윈도우기준
     end:'100% 80%',
     markers:true,  //화면에 가이드선 표시
     id:'박스5',
-    scrub:1,
+    // scrub:1,
     },
     y:200,
     opacity:0,
