50097b59b8e12777a5c1e1dd8b965b1637802c02
rustymotor <vmfpdlasz@gmail.com>
2024-03-29 10:16:42 +0900

    29

--- script/scroll.js
+++ script/scroll.js
@@ -84,3 +84,34 @@ ScrollTrigger.create({
 	// pin:true,
 	// anticipatePin:1,
 });
+//숫자 세기
+var counted = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#count').offset().top - window.innerHeight;
+  if (counted == 0 && $(window).scrollTop() > oTop) {
+    $('#count').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+        {
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    counted = 1;
+  }
+
+});
